6436d96791028786f2454a0286ae17833af92d2f
240280 <240280@DESKTOP-9G513UE.lan>
2017-04-27 20:47:23 +0200

    34tw

--- DataLayer/src/Box.java
+++ DataLayer/src/Box.java
@@ -20,7 +20,7 @@ public class Box {
 	public String getItemType() {
 		return itemType;
 	}
-	//skurvenyleonajqwdqwerg
+	//skurvenyleonajqwdqwerjkg
 	public void setItemType(String itemType) {
 		this.itemType = itemType;
 	}
