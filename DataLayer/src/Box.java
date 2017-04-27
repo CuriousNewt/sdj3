14ef6ff6410a04cc96329aeb6e6937fcf23db228
240280 <240280@192.168.1.194>
2017-04-27 21:02:18 +0200

    KURVAAA

--- DataLayer/src/Box.java
+++ DataLayer/src/Box.java
@@ -20,7 +20,7 @@ public class Box {
 	public String getItemType() {
 		return itemType;
 	}
-	//skurvenyleonajqwdqwjkghbhnnjqwdwanyad
+	//skurvenyleonajqwdqwjkghbhnnjqwdwanyadg
 	public void setItemType(String itemType) {
 		this.itemType = itemType;
 	}
